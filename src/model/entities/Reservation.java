f76317d911cc4e83b2a5a297af3a3e71111f0497
VitorSantosH <wytorh@gmail.com>
2021-12-16 07:12:58 -0300

    Sulução boa

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer rooNumber;
@@ -14,6 +16,17 @@ public class Reservation {
 
 	public Reservation(Integer rooNumber, Date checkIn, Date checkOut) {
 
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+
+			throw new DomainException("Reservation dates for update must be fyture dates"); 
+		}
+
+		if (!checkOut.after(checkIn)) {
+			
+			throw new DomainException("Error in reservation: Check-out date must be after check-in date");
+			
+		}
 		this.rooNumber = rooNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -41,26 +54,25 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void  updateDates(Date checkIn, Date checkOut)   {
 
 		Date now = new Date();
 
 		if (checkIn.before(now) || checkOut.before(now)) {
 
-			return "Reservation dates for update must be fyture dates";
+			throw new DomainException("Reservation dates for update must be fyture dates"); 
 		}
 
 		if (!checkOut.after(checkIn)) {
 			
-			return "Error in reservation: Check-out date must be after check-in date";
+			throw new DomainException("Error in reservation: Check-out date must be after check-in date");
 			
 		}
 
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 		
-		return null;
-
+		
 	}
 
 	@Override
